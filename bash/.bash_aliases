e13b74e9987e678174ce827b191b75d6fc7359fe
DavidWuthier <dwuthier@gmail.com>
2021-07-24 23:17:23 +0200

    Fix aliases

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -1,3 +1,5 @@
 
 alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
+alias brc='vim ~/install/bash/.bash_custom'
+alias ba='vim ~/install/bash/.bash_aliases'
 
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 #sudo apt install build-essential cmake python3-dev -y
 sudo apt install terminator -y
 sudo apt install vim vim-gtk3 -y
+sudo apt install net-tools -y
 #sudo apt install git -y
 #sudo apt install nautilus-dropbox -y
 #sudo apt install python3-pip -y
